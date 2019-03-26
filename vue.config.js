fd0ef83879878bdd2cfa4f8cc95eb84b0e499c4d
rsghost <im@rsghost.org>
2019-03-26 18:20:09 +0800

    update(vue):google map key & build setting

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,27 @@
+const config = require('./config.js')
+const GOOGLE_MAP_KEY = config.key.googleMap
+const BASE_URL = config.addr
+console.log('GOOGLE_MAP_KEY:' + GOOGLE_MAP_KEY)
+console.log('BASE_URL:' + BASE_URL)
+
 module.exports = {
-  baseUrl: '/storymap/',
-  configureWebpack: config => {
-    config.externals = {
+  publicPath: BASE_URL,
+  configureWebpack: cfg => {
+    cfg.externals = {
       'gapi': 'gapi',
       'VCO': 'VCO',
       'L': 'L',
       'L1': 'L1',
       'stamen': 'stamen'
     }
+  },
+  chainWebpack: cfg => {
+    cfg.plugin('html').tap(args => {
+      args[0].templateParameters = {
+        GOOGLE_MAP_KEY,
+        BASE_URL
+      }
+      return args
+    })
   }
 }
